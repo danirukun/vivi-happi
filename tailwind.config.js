9c3bfc10e1f0c5efad0fbafb8063dac0a471f511
Daniils Petrovs <dan@danirukun.com>
2025-11-24 16:48:08 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        'vivi-pink': {
+          '50': '#fcf3f8',
+          '100': '#fbe8f2',
+          '200': '#f8d2e6',
+          '300': '#f4add1',
+          '400': '#ee8cbb',
+          '500': '#e15393',
+          '600': '#cf3371',
+          '700': '#b32359',
+          '800': '#94204a',
+          '900': '#7c1f40',
+          '950': '#4b0c22',
+        },
+        'vivi-purple': {
+          '50': '#fbf6fe',
+          '100': '#f5ebfc',
+          '200': '#eddafa',
+          '300': '#dfbdf5',
+          '400': '#cb93ed',
+          '500': '#c07de6',
+          '600': '#a34ad3',
+          '700': '#8c38b8',
+          '800': '#763297',
+          '900': '#602a79',
+          '950': '#431358',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
+
